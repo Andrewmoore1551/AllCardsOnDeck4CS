94c03ef970d2f2d3610ea0884a17fd637ee1bcd3
Andrew Moore <mooreandrew015@gmail.com>
2021-04-14 18:37:54 -0400

    created three lists for a deck of cards

--- Program.cs
+++ Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AllCardsOnDeck4CS
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            var suit = new List<string>() { "Clubs", "Hearts", "Diamonds", "Spades" };
+            var listOfCards = new List<string>();
+            var cardValue = new List<string>() { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+
+
+
+        }
     }
-  }
 }
