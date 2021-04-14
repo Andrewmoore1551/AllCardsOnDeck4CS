22e0de0a6a689f2eb7f4be3f2c23e8df7a7a4c67
Andrew Moore <mooreandrew015@gmail.com>
2021-04-14 18:39:23 -0400

    defined index for card values

--- Program.cs
+++ Program.cs
@@ -11,6 +11,11 @@ namespace AllCardsOnDeck4CS
             var listOfCards = new List<string>();
             var cardValue = new List<string>() { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
 
+            for (int index = 0; index < cardValue.Count; index++)
+            {
+                for (int j = 0; j < suit.Count; j++)
+                    listOfCards.Add($"{cardValue[index]} of {suit[j]}");
+            };
 
 
         }
